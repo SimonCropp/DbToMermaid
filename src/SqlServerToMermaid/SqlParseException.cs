bc15345cf17f4df284e808fa2cc0210c3159f66c
Simon Cropp <simon.cropp@gmail.com>
2026-03-01 20:47:12 +1100

    Update SqlParseException.cs

--- src/SqlServerToMermaid/SqlParseException.cs
+++ src/SqlServerToMermaid/SqlParseException.cs
@@ -1,15 +1,24 @@
 ﻿namespace DbToMermaid;
 
+/// <summary>
+/// Thrown when a SQL script contains syntax errors that prevent parsing.
+/// </summary>
+/// <param name="errors">The parse errors reported by the SQL parser.</param>
 public class SqlParseException(IList<ParseError> errors) :
     Exception
 {
+    /// <summary>
+    /// The parse errors reported by the SQL parser.
+    /// </summary>
     public IList<ParseError> Errors { get; } = errors;
 
+    /// <inheritdoc />
     public override string ToString()
     {
         var messages = string.Join(Environment.NewLine, Errors.Select(_ => $"Line {_.Line}: {_.Message}"));
         return $"SQL parse errors:{Environment.NewLine}{messages}";
     }
 
+    /// <inheritdoc />
     public override string Message => ToString();
 }
