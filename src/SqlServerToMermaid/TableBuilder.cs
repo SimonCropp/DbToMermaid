c42b92b5770a1300b67b1dcf2c060273a2a0cfe8
Simon Cropp <simon.cropp@gmail.com>
2026-03-23 10:58:31 +1100

    fix Re-sorting columns every render

--- src/SqlServerToMermaid/TableBuilder.cs
+++ src/SqlServerToMermaid/TableBuilder.cs
@@ -9,11 +9,15 @@
 
     public Table Build()
     {
-        var columns = Columns.Select(_ =>
-            ColumnComments.TryGetValue(_.Name, out var comment)
-                ? _ with { Comment = comment }
-                : _
-        ).ToList();
-        return new(Schema, Name, columns, PrimaryKeys.Count > 0 ? PrimaryKeys : null, Comment);
+        var pks = PrimaryKeys.Count > 0 ? PrimaryKeys : null;
+        var columns = Columns
+            .OrderBy(_ => pks?.Contains(_.Name) != true)
+            .ThenBy(_ => _.Ordinal)
+            .Select(_ =>
+                ColumnComments.TryGetValue(_.Name, out var comment)
+                    ? _ with { Comment = comment }
+                    : _)
+            .ToList();
+        return new(Schema, Name, columns, pks, Comment);
     }
 }
